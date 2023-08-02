b6b99a0cfa960eb235a3b8db66e78af95132b245
Vinicius Sato <vinicius.sato@outlook.com>
2023-08-02 20:48:08 -0300

    salvando dados das despesas

--- app/src/main/java/com/example/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/example/organizze/activity/DespesasActivity.java
@@ -3,16 +3,19 @@ package com.example.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.organizze.R;
 import com.example.organizze.helper.DateCustom;
+import com.example.organizze.model.Movimentacao;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,17 @@ public class DespesasActivity extends AppCompatActivity {
 
         //Preenche o campo data com a data atual
         campoData.setText(DateCustom.dataAtual());
+    }
+
+    public void salvarDespesa(View view) {
+        movimentacao = new Movimentacao();
+        String data = campoData.getText().toString();
+        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(campoDescricao.getText().toString());
+        movimentacao.setData(data);
+        movimentacao.setTipo("d");
+
+        movimentacao.salvar(data);
     }
 }

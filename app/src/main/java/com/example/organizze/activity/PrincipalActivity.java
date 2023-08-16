1bb3189a8b4d41a0d02495bee517a3370f267373
Vinicius Sato <vinicius.sato@outlook.com>
2023-08-15 22:39:17 -0300

    Adicionando calendario

--- app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.organizze.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textSaldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        textSaldo = findViewById(R.id.textSaldo);
+        textoSaudacao = findViewById(R.id.textSaudacao);
+        calendarView = findViewById(R.id.calendarView);
+        configuraCalendarView();
 
 
         /*
@@ -33,5 +45,17 @@ public class PrincipalActivity extends AppCompatActivity {
     }
     public void adicionarReceita(View view){
         startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void configuraCalendarView() {
+        CharSequence meses[] = {"Janeiro","Fevereio","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 }
